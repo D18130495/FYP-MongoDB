6ea5231335e6ea5b5f0d6a3dc33b4b88e8648ee8
Yushun Zeng <d18130495@mytudublin.ie>
2022-10-27 14:53:37 +0100

    Merged datasets to a single csv file

--- movie.py
+++ movie.py
@@ -1,9 +1,46 @@
+import numpy as np
 import pandas as pd
 import json
 
-from bson import json_util
-
 from mongodbConnection import connect_mongo
+
+
+def merge_data(movies, ratings, links, merged_tags):
+    datas = pd.merge(movies, ratings, on='movieId')
+    datas = pd.merge(datas, links, on='movieId')
+    datas = pd.merge(datas, merged_tags, on=['movieId', 'userId'])
+    datas = datas[['movieId', 'imdbId', 'tmdbId', 'title', 'genres', 'userId', 'rating', 'tags']].\
+        sort_values(by=['movieId', 'userId'], ascending=True)
+
+    for line in range(len(datas)):
+        if datas["title"][line][-12:-7] == ", The":
+            datas.loc[line, "title"] = "The " + datas["title"][line].replace(", The", "")
+
+    datas.to_csv('Datasets/movie/datas.csv', index=False)
+
+
+def merge_tags(tags):
+    user_id = tags.iloc[0]["userId"]
+    movie_id = tags.iloc[0]["movieId"]
+
+    tags_array = []
+    merged_tags = pd.DataFrame(columns=['userId', 'movieId', 'tags'])
+
+    for line in range(len(tags)):
+        if tags["userId"][line] == user_id and tags["movieId"][line] == movie_id:
+            tags_array.append(tags.loc[line, "tag"])
+        else:
+            arr = np.array(tags_array)
+            tags_str = ', '.join(arr)
+            merged_tags = merged_tags.append({'userId': user_id, 'movieId': movie_id, 'tags': tags_str}, ignore_index=True)
+
+            tags_array.clear()
+            user_id = tags.loc[line, "userId"]
+            movie_id = tags.loc[line, "movieId"]
+
+            tags_array.append(tags["tag"][line])
+
+    merged_tags.to_csv('Datasets/movie/mergedTags.csv', index=False)
 
 
 def create_document(df, col):
@@ -14,25 +51,40 @@ def create_document(df, col):
 
         agg_info = this_movie[['movieId', 'title', 'genres']]
 
+        tc = this_movie[['userId', 'rating']]
+
         entries = json.dumps({
             "MovieID": str(i[0]),
             "Title": agg_info['title'].values[0],
-            "Genres": agg_info['genres'].values[0]
-            # "Athletes": tc.to_dict('records')
+            "Genres": agg_info['genres'].values[0],
+            "Athletes": tc.to_dict('records')
         })
-        # print(json.loads(entries))
+
         col.insert_one(json.loads(entries))
 
 
 def main():
     col = connect_mongo("fyp", "movie")
-    df = pd.read_csv('Datasets/movie/movies.csv', sep=',', delimiter=None, encoding='latin-1')
-    create_document(df, col)
-    # mydict = {"name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com"}
-    # col.insert_one(mydict)
+    movies = pd.read_csv('Datasets/movie/movies.csv', sep=',', delimiter=None, encoding='latin-1')
+    ratings = pd.read_csv("Datasets/movie/ratings.csv", sep=',', delimiter=None, encoding='latin-1')
+    links = pd.read_csv("Datasets/movie/links.csv", sep=',', delimiter=None, encoding='latin-1')
+
+    # create merged tags csv if it not exits
+    try:
+        merged_tags = pd.read_csv("Datasets/movie/mergedTags.csv", sep=',', delimiter=None, encoding='latin-1')
+    except FileNotFoundError:
+        tags = pd.read_csv("Datasets/movie/tags.csv", sep=',', delimiter=None, encoding='latin-1')
+        merge_tags(tags)
+        merged_tags = pd.read_csv("Datasets/movie/mergedTags.csv", sep=',', delimiter=None, encoding='latin-1')
+
+    # merged dataset if datas.csv is not exits
+    try:
+        datas = pd.read_csv("Datasets/movie/datas.csv", sep=',', delimiter=None, encoding='latin-1')
+    except FileNotFoundError:
+        merge_data(movies, ratings, links, merged_tags)
+
+    # create_document(datas, col)
 
 
 if __name__ == '__main__':
     main()
-
-    # agginfo = movie[['ID', 'ImdbID', 'TmdbID', 'Title', 'Year', 'URL', 'Director', 'Actor', 'Genres', 'Rate']]
